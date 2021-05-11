60a632682c8da71b8538719bbee67476bdfac3c0
Hung Nguyen <hunglnl01@gmail.com>
2021-05-10 23:05:29 -0700

    delete table and add posts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,11 +33,32 @@ app.get('/createdb', (req: any, res: any) => {
 // Create table
 app.get('/createpoststable', (req: any, res: any) => {
 	let sql =
-		'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), PRIMARY KEY(id))';
+		'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
 	db.query(sql, (err: any, result: any) => {
 		if (err) throw err;
 		console.log(result);
 		res.send('Posts table created');
+	});
+});
+
+// Delete table
+app.get('/droppoststable', (req: any, res: any) => {
+	let sql = 'DROP TABLE posts';
+	db.query(sql, (err: any, result: any) => {
+		if (err) throw err;
+		console.log(result);
+		res.send('Posts table deleted');
+	});
+});
+
+// Insert post 1
+app.get('/addpost1', (req: any, res: any) => {
+	let post = { title: 'Post One', body: 'This is post number 1' };
+	let sql = 'INSERT INTO posts SET?';
+	let query = db.query(sql, post, (err: any, result: any) => {
+		if (err) throw err;
+		console.log(result);
+		res.send('post 1 added');
 	});
 });
 
