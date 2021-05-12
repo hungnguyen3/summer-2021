20dfb8b92152d3a17699e8a4148619e1ab45224b
Hung Nguyen <hunglnl01@gmail.com>
2021-05-11 23:35:48 -0700

    body parser & select posts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 require('dotenv').config();
+var bodyParser = require('body-parser');
 
 // Create connection
 const db = mysql.createConnection({
@@ -19,6 +20,12 @@ db.connect((err: any) => {
 });
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // Create DB
 app.get('/createdb', (req: any, res: any) => {
@@ -51,14 +58,25 @@ app.get('/droppoststable', (req: any, res: any) => {
 	});
 });
 
-// Insert post 1
-app.get('/addpost1', (req: any, res: any) => {
-	let post = { title: 'Post One', body: 'This is post number 1' };
+// Insert post
+app.post('/addpost', (req: any, res: any) => {
+	console.log(req);
+	let post = { id: req.body.id, title: req.body.title, body: req.body.body };
 	let sql = 'INSERT INTO posts SET?';
 	let query = db.query(sql, post, (err: any, result: any) => {
 		if (err) throw err;
 		console.log(result);
-		res.send('post 1 added');
+		res.send('post added');
+	});
+});
+
+// Select post
+app.get('/getposts', (req: any, res: any) => {
+	let sql = 'SELECT * FROM posts';
+	let query = db.query(sql, (err: any, result: any) => {
+		if (err) throw err;
+		console.log(result);
+		res.send('posts fetched');
 	});
 });
 
