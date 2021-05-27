c15e76c03fb4c9abf7d85870c68fea041f72346d
Hung Nguyen <hunglnl01@gmail.com>
2021-05-26 23:58:15 -0700

    react practice

---
+++ frontend/src/components/CuteCat.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CuteCat extends Component {
+    render() {
+        return (
+            <h1>
+                cute cat saying hello
+            </h1>
+        )
+    }
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import CuteCat from './components/CuteCat'
+import CuteDog from './components/CuteDog'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
+		<CuteCat/>
+		<CuteDog/>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
